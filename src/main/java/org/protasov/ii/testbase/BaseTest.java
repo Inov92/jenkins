870977ede547cf186a078de2ab387eb8937429fc
Ivan Protasov <pro-i@bk.ru>
2017-06-25 13:54:38 +0300

    Allure annotation added

--- src/main/java/org/protasov/ii/testbase/BaseTest.java
+++ src/main/java/org/protasov/ii/testbase/BaseTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.annotations.*;
+import ru.yandex.qatools.allure.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,19 +20,22 @@ public abstract class BaseTest {
     protected static WebDriver driver;
 
     @BeforeSuite
+    @Step("=====Browser Session Started=====")
     protected void getFireFoxWebDriver(){
-        Reporter.log("=====Browser Session Started=====", true);
+        //Reporter.log("=====Browser Session Started=====", true);
         TestSettings settings = new TestSettings();
         System.setProperty(settings.getDriverName(), settings.getDriverPath());
         driver = new FirefoxDriver();
         Reporter.log("=====WebDriver created=====", true);
     }
+    @Step("=====WebDriver created=====")
 
     @AfterSuite(alwaysRun = true)
     protected void closeDriver(){
         driver.close();
         Reporter.log("=====WebDriver closed=====", true);
     }
+    @Step("=====WebDriver closed=====")
     @AfterMethod
     //созранять в лог testng
     public void takeScreenShotOnFailure(ITestResult testResult) throws IOException {
--- src/test/java/org/protasov/ii/lesson3/tests/LoginTest.java
+++ src/test/java/org/protasov/ii/lesson3/tests/LoginTest.java
@@ -5,13 +5,18 @@ import org.protasov.ii.testbase.*;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.*;
+import ru.yandex.qatools.allure.annotations.*;
+import ru.yandex.qatools.allure.model.SeverityLevel;
 
+@Title("This is test suit for checking authentication functionality")
 public class LoginTest extends BaseTest {
 
+    @Title("Positive login test")
+    @Description("User try log in with correct login and password")
+    @Severity(SeverityLevel.BLOCKER)
+    @Step("=====validLoginTest started=====")
     @Test(dataProvider = "dataFromMethod", dataProviderClass = DataProviders.class)
     public void validLoginTest(String login, String password){
-
-        Reporter.log("=====validLoginTest started=====", true);
 
             loadPage("https://jdi-framework.github.io/tests/index.htm");
 
@@ -20,11 +25,12 @@ public class LoginTest extends BaseTest {
                 registerUser(login, password).
                 pressEnterToLogin().isProfileNameVisible());
     }
-
+    @Title("Negative login test")
+    @Description("User try log in with incorrect login and password")
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("=====invalidLoginTest started=====")
     @Test(dataProvider = "dataFromCSV", dataProviderClass = DataProviders.class)
     public  void invalidLoginTest(String login, String password){
-
-        Reporter.log("=====invalidLoginTest started=====", true);
 
         loadPage("https://jdi-framework.github.io/tests/index.htm");
         Assert.assertTrue(LoginPanel.init().
